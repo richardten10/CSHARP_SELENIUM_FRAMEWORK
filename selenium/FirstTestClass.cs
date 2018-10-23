9ea65339c649c3424ecf92b8811cb5d285891e9d
richardten10 <richard.rawson@ten10.com>
2018-10-23 19:23:43 +0100

    Add project files.

---
+++ selenium/FirstTestClass.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace selenium
+{
+    class FirstTestClass
+    {
+        static DriverFactory driverFactory = new DriverFactory();
+        static IWebDriver driver = driverFactory.getDriver();
+        FirstPage firstpage = new FirstPage(driver);
+
+        [SetUp]
+        public void Initialize()
+        {
+            firstpage.goTo(); 
+            Console.WriteLine("Opened URL");
+        }
+
+
+        [Test]
+        public void ExecuteTest()
+        {
+            firstpage.searchForTrainline();
+            Console.WriteLine("searched");
+
+            firstpage.clickOnSite();
+            Console.WriteLine("clicked on site");
+
+            Assert.AreEqual("Trainline | Buy Cheap Train Tickets | Live Train Times & Fares", driver.Title);
+            Console.WriteLine("Page title is correct");
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("Next method started");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+           
+            driver.Close();
+            Console.WriteLine("Closed the browser");
+        }
+    }
+}
---
+++ selenium/firstPage.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium
+{
+    class FirstPage : BasePage
+    {
+
+        public FirstPage(IWebDriver driver): base(driver)
+        {
+           PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "q")]
+        public IWebElement searchBar { get; set; }
+
+
+        [FindsBy(How = How.CssSelector, Using = "[data-hveid='CAQQAQ'] .LC20lb")]
+        public IWebElement trainlineLink { get; set; }
+
+
+        [FindsBy(How = How.Id, Using = "TitleId")]
+        public IWebElement TitleID3 { get; set; }
+
+
+        [FindsBy(How = How.Id, Using = "TitleId")]
+        public IWebElement TitleID4 { get; set; }
+
+        public void goTo()
+        {
+            driver.Navigate().GoToUrl("https:www.google.co.uk");
+        }
+
+        public void searchForTrainline()
+        {
+            enterText(searchBar, "trainline");
+            pressEnter();  
+        }
+
+        public void clickOnSite()
+        {
+            waitAndClick(trainlineLink);
+        }
+
+
+    }
+}
